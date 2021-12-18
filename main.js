6ecc789b0be45e14d08473a4551079ac9e2d1f38
Infinity101-jpg <68457668+Infinity101-jpg@users.noreply.github.com>
2021-12-18 15:38:59 +0200

    Create main.js

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ token = 'OTE3NDk5OTMzODExMTc1NDY0.Ya5mZg.8BGx7DZijoj_PMkmLQVqWab3o-c'
 prefix = '_'
 bot.run(token, prefix)
 
-
+O-T-E-3-N-D-k-5-O-T-M-z-O-D-E-x-M-T-c-1-NDY0.Ya5mZg.8BGx7DZijoj_PMkmLQVqWab3o-c
 
