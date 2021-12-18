6552190a4ad393fb904fda1a95b18b4add3f3149
Infinity101-jpg <68457668+Infinity101-jpg@users.noreply.github.com>
2021-12-18 15:31:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const bot = require('./bot')
-token = 'OTE3NDk5OTMzODExMTc1NDY0.Ya5mZg.VQiyVnBdt1BUVCwVrqPARh8yj5w'
+token = 'OTE3NDk5OTMzODExMTc1NDY0.Ya5mZg.8BGx7DZijoj_PMkmLQVqWab3o-c'
 prefix = '_'
 bot.run(token, prefix)
 
